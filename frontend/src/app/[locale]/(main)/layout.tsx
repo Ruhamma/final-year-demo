9dc528d9f00f2fe9006be4224b71063c95efa489
Rediat <rediathmariam2@gmail.com>
2025-04-08 10:41:04 +0300

    feat: add header and footer

---
+++ frontend/src/app/[locale]/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "@/shared/components/footer";
+import { Header } from "@/shared/components/header";
+import type { ReactNode } from "react";
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) => {
+  return (
+    <div className="w-full h-full  ">
+      <Header />
+      <main className="w-full min-h-screen mt-6">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ frontend/src/shared/components/header/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Card,
+  Stack,
+  Flex,
+  UnstyledButton,
+  ActionIcon,
+  Image,
+} from "@mantine/core";
+import Link from "next/link";
+import { IconShoppingCart, IconHeart } from "@tabler/icons-react";
+
+export function Header() {
+  return (
+    <Card bg={"#fffcea"}>
+      <Box className="container mx-auto">
+        <Stack>
+          <Flex justify="space-between" align="center">
+            <Flex justify="space-between" align="center"gap={8}>
+              <UnstyledButton className="hover:border-b-[1px] tracking-[0.2em]">AM</UnstyledButton>
+              {"/"}
+              <UnstyledButton className="hover:border-b-[1px] tracking-[0.2em]">EN</UnstyledButton>
+            </Flex> 
+            <Image  src={'/logo.png'} alt="Logo" width={72} height={43} />
+            <Flex justify="space-between" align="center" py={10} gap={20}>
+            <Link href={"#"} className="tracking-[0.3em] hover:border-b-[1px]">Sign Up</Link>
+            <ActionIcon
+              variant="transparent"
+              size="lg"
+              radius="xl"
+              color="dark"
+              mr={5}
+            >
+              <IconShoppingCart size={20} stroke={1.5} />
+            </ActionIcon>
+            <ActionIcon
+              variant="transparent"
+              size="lg"
+              radius="xl"
+              color="dark"
+              mr={5}
+            >
+              <IconHeart size={20} stroke={1.5} />
+            </ActionIcon>
+          </Flex>
+          </Flex>
+         <Flex align={'center'} justify={'space-between'}>
+         <Flex gap={50}>
+           <Link href={"#"} className=" tracking-[0.3em] hover:border-b-[1px]">Home</Link>
+           <Link href={"#"}  className=" tracking-[0.3em] hover:border-b-[1px]">About</Link>
+           <Link href={"#"}  className=" tracking-[0.3em] hover:border-b-[1px]">Contact</Link>
+         </Flex>
+         <Flex gap={50}>
+           <Link href={"#"} className=" tracking-[0.3em] hover:border-b-[1px]">Artworks</Link>
+           <Link href={"#"} className=" tracking-[0.3em] hover:border-b-[1px]">Artist</Link>
+         </Flex>
+         </Flex>
+        </Stack>
+      </Box>
+    </Card>
+  );
+}
